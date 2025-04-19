466f78e78e99acaffd1e1b5a1809d6297c1dd441
Dacst12 <misiya3@yandex.ru>
2025-04-19 21:54:09 +0500

    доработает шаблон редактирования точки маршрута

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -1,10 +1,10 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import {POINT_TYPES} from '../const.js';
 import {formatToFullDate, humanizeTime, capitalizeFirstLetter} from '../utils/waypoints.js';
 
-const createEventTypeTemplate = (type) => (`
+const createEventTypeTemplate = (type, checkedAttribute) => (`
   <div class="event__type-item">
-    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${checkedAttribute}>
     <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${capitalizeFirstLetter(type)}</label>
   </div>
 `);
@@ -13,10 +13,14 @@ const createDestinationsListTemplate = (destination) => (`
   <option value=${destination}></option>
 `);
 
-const createOfferTemplate = ({title, price}, checkedAttribute) => (`
+const createDestinationImageTemplate = ({src, description}) => (`
+  <img class="event__photo" src="${src}" alt="${description}">
+`);
+
+const createOfferTemplate = ({id, title, price}, checkedAttribute) => (`
   <div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${checkedAttribute}>
-    <label class="event__offer-label" for="event-offer-luggage-1">
+    <input class="event__offer-checkbox  visually-hidden" id="${id}" type="checkbox" name="event-offer-luggage" ${checkedAttribute}>
+    <label class="event__offer-label" for="${id}">
       <span class="event__offer-title">${title}</span>
       &plus;&euro;&nbsp;
       <span class="event__offer-price">${price}</span>
@@ -24,7 +28,7 @@ const createOfferTemplate = ({title, price}, checkedAttribute) => (`
   </div>
 `);
 
-const createEditWaypointTemplate = (point, {offers}, {description, name}, destinationsList) => {
+const createEditWaypointTemplate = (point, {offers}, {name, description, pictures}, destinationsList) => {
   const {basePrice, dateFrom, dateTo, offers : offersPoint, type} = point;
 
   return (`
@@ -42,7 +46,11 @@ const createEditWaypointTemplate = (point, {offers}, {description, name}, destin
               <fieldset class="event__type-group">
                 <legend class="visually-hidden">Event type</legend>
 
-                ${POINT_TYPES.map((item) => createEventTypeTemplate(item)).join('')}
+    ${POINT_TYPES.map((item) => {
+      const checkedAttribute = item === type ? 'checked' : '';
+      return createEventTypeTemplate(item, checkedAttribute);
+    }).join('')}
+
               </fieldset>
             </div>
           </div>
@@ -59,7 +67,7 @@ const createEditWaypointTemplate = (point, {offers}, {description, name}, destin
 
           <div class="event__field-group  event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value=${formatToFullDate(dateFrom)} ${humanizeTime(dateFrom)}>
+            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formatToFullDate(dateFrom)} ${humanizeTime(dateFrom)}">
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
             <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formatToFullDate(dateTo)} ${humanizeTime(dateTo)}">
@@ -80,30 +88,37 @@ const createEditWaypointTemplate = (point, {offers}, {description, name}, destin
           </button>
         </header>
         <section class="event__details">
-          <section class="event__section  event__section--offers">
-            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+          ${offers.length !== 0 ? `<section class="event__section  event__section--offers">
+              <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
-            <div class="event__available-offers">
+              <div class="event__available-offers">
 
     ${offers.map((offer) => {
       const checkedAttribute = offersPoint.includes(offer.id) ? 'checked' : '';
       return createOfferTemplate(offer, checkedAttribute);
     }).join('')}
 
-            </div>
-          </section>
+              </div>
+            </section>` : ''}
 
-          <section class="event__section  event__section--destination">
+          ${description || pictures.length !== 0 ? `<section class="event__section  event__section--destination">
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-            <p class="event__destination-description">${description}</p>
-          </section>
+            ${description ? `<p class="event__destination-description">${description}</p>` : ''}
+
+            ${pictures.length !== 0 ? `<div class="event__photos-container">
+              <div class="event__photos-tape">
+                ${pictures.map((picture) => createDestinationImageTemplate(picture)).join('')}
+              </div>
+            </div>` : ''}
+
+          </section>` : ''}
         </section>
       </form>
     </li>
   `);
 };
 
-export default class EditWaypointView extends AbstractView {
+export default class EditWaypointView extends AbstractStatefulView {
   #point = null;
   #offers = null;
   #destination = null;
